b4fae1efe3b3d6e456dbd9a29e416b3c867376b3
wangzhicheng <wangzhicheng@chelun.com>
2020-08-10 18:03:09 +0800

    动态拷贝

--- WCTableSource/Classes/DTTableRowGroup.m
+++ WCTableSource/Classes/DTTableRowGroup.m
@@ -6,6 +6,7 @@
 //
 
 #import "DTTableRowGroup.h"
+#import <objc/runtime.h>
 
 @implementation DTTableRowGroup
 
@@ -14,26 +15,38 @@
     return [self new];
 }
 
-- (DTTableRow *)getRowWithIndex:(NSInteger)index
+- (DTTableRow *)createRowWithIndex:(NSInteger)index
 {
     DTTableRow *row = [DTTableRow new];
-    row.reuseCellId = self.reuseCellId;
-    row.cellClass = self.cellClass;
-    row.cellBlock = self.cellBlock;
-    row.isXibCell = self.isXibCell;
-    row.actionType = self.actionType;
-    row.userInfo = self.userInfo;
-    row.heightBlock = self.heightBlock;
-    row.initBlock = self.initBlock;
-    row.configBlock = self.configBlock;
-    row.clickBlock = self.clickBlock;
-    row.dataMethod = self.dataMethod;
-    row.autoSetItem = self.autoSetItem;
+    //动态拷贝group的属性 到 row
+    // 1.获取属性列表
+    unsigned int propertyCount = 0;
+    objc_property_t * propertyArray = class_copyPropertyList([row class], &propertyCount);
+    for (int i=0; i<propertyCount; i++) {
+        objc_property_t  property = propertyArray[i];
+
+        // 2.属性名字
+        const char * propertyName = property_getName(property);
+        NSString * key = [NSString stringWithUTF8String:propertyName];
+        
+        // 3.通过属性名拿到属性值
+        id value=[self valueForKey:key];
+        
+        // 4.判断 值对象是否响应copyWithZone
+        if ([value respondsToSelector:@selector(copyWithZone:)]) {
+            //5. 设置属性值
+            [row setValue:[value copy] forKey:key];
+        } else {
+            [row setValue:value forKey:key];
+        }
+    }
+    //*****切记需要手动释放
+    free(propertyArray);
     
     row.cell = [self cellWithIndex:index];
     row.data = [self dataWithIndex:index];
     row.height = [self heightWithIndex:index];
-    return self;
+    return row;
 }
 
 - (UITableViewCell *)cellWithIndex:(NSInteger)index
@@ -69,7 +82,7 @@
         NSMutableArray *array = [NSMutableArray array];
         NSInteger count = MAX(self.dataList.count, self.cellList.count);
         for (int i=0; i<count; i++) {
-            [array addObject:[self getRowWithIndex:i]];
+            [array addObject:[self createRowWithIndex:i]];
         }
         return array;
     } else {
